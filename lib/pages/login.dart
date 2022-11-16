badec90b9005a6311273bc8756f120d757306373
reham611 <r.ramadan.ict@btu.edu.eg>
2022-11-16 13:00:12 -0800

    new

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import '../shared/custom_textfield.dart';
+
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -16,45 +18,19 @@ class Login extends StatelessWidget {
               const SizedBox(
                 height: 64,
               ),
-              TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Email : ",
-                    // To delete borders
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 220, 18, 18),
-                      ),
-                    ),
-                    // fillColor: Colors.red,
-                    filled: true,
-                    contentPadding: const EdgeInsets.all(8),
-                  )),
+              MyTextField(
+                textInputTypee: TextInputType.emailAddress,
+                ispassword: false,
+                hinttexttt: "Enter Your Email : ",
+              ),
               const SizedBox(
                 height: 33,
               ),
-              TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    hintText: "Enter Your Email : ",
-                    // To delete borders
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: Divider.createBorderSide(context),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Color.fromARGB(255, 220, 18, 18),
-                      ),
-                    ),
-                    // fillColor: Colors.red,
-                    filled: true,
-                    contentPadding: const EdgeInsets.all(8),
-                  ))
+              MyTextField(
+                textInputTypee: TextInputType.text,
+                ispassword: true,
+                hinttexttt: "Enter Your Password : ",
+              ),
             ],
           ),
         ),
--- lib/shared/custom_textfield.dart
+++ lib/shared/custom_textfield.dart
@@ -3,22 +3,30 @@
 import 'package:flutter/material.dart';
 
 class MyTextField extends StatelessWidget {
-  const MyTextField({super.key});
+  final TextInputType textInputTypee;
+  final bool ispassword;
+  final String hinttexttt;
+
+  MyTextField({
+    required this.textInputTypee,
+    required this.ispassword,
+    required this.hinttexttt,
+  });
 
   @override
   Widget build(BuildContext context) {
     return TextField(
-        keyboardType: TextInputType.emailAddress,
-        obscureText: false,
+        keyboardType: textInputTypee,
+        obscureText: ispassword,
         decoration: InputDecoration(
-          hintText: "Enter Your Email : ",
+          hintText: hinttexttt,
           // To delete borders
           enabledBorder: OutlineInputBorder(
             borderSide: Divider.createBorderSide(context),
           ),
           focusedBorder: OutlineInputBorder(
             borderSide: BorderSide(
-              color: Colors.grey,
+              color: Color.fromARGB(255, 231, 27, 27),
             ),
           ),
           // fillColor: Colors.red,
