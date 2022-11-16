7de5558ff064b3e1b1ed50b566e150ad3018e9e0
reham611 <r.ramadan.ict@btu.edu.eg>
2022-11-16 10:45:59 -0800

    creat folder (shared) and add Textfield

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
@@ -7,8 +9,54 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: [Text("data")],
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 220, 18, 18),
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
+                  )),
+              const SizedBox(
+                height: 33,
+              ),
+              TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  obscureText: false,
+                  decoration: InputDecoration(
+                    hintText: "Enter Your Email : ",
+                    // To delete borders
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 220, 18, 18),
+                      ),
+                    ),
+                    // fillColor: Colors.red,
+                    filled: true,
+                    contentPadding: const EdgeInsets.all(8),
+                  ))
+            ],
+          ),
         ),
       ),
     );
