ef932ee7c4092acb4ccbf552e4a82f0e4d745186
reham611 <r.ramadan.ict@btu.edu.eg>
2022-11-17 05:58:21 -0800

    done

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flower_app/shared/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,22 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        actions: [Text("\$ 13")],
+        // ignore: prefer_const_literals_to_create_immutables
+        actions: [
+          Row(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text(
+                  "\$ 13",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ],
         backgroundColor: appbarGreen,
         title: Text("data"),
       ),
